450e72b0d48ff08991771a62453feb8821381058
Jessica Prades <filaksofias@gmail.com>
2017-01-24 09:31:15 -0500

    Completed version

--- weather.js
+++ weather.js
@@ -2,7 +2,9 @@
  * Created by jesica on 10/30/2016.
  */
 
-
+var mainTemp;
+var minTemp;
+var maxTemp;
 /*ACCESS TO WEATHER API*/
 
 function position(geo) {
@@ -21,64 +23,101 @@ function position(geo) {
 function setTime() {
 
     $("#dateStyle").html(new Date().toLocaleString());
+    setTimeout(setTime, 1000);
+}
+
+
+/*CHANGE CELSIUS//FAHRENHEIT*/
+
+function changeUnits() {
+
+
+    // if ($("#celsButton").prop("checked") == true) {
+    if ($("#celsButton").text() == "°C") {
+
+        console.log("cambiando a celsius");
+
+        $("#celsButton").html("°F");
+
+        $("#highTemp").html(maxTemp + "°C");
+        $("#lowTemp").html(minTemp + "°C");
+        $(".temperatureStyle").html(mainTemp + "°C");
+
+    } else {
+
+        console.log("cambiando a fahrenheit");
+        $("#celsButton").html("°C");
+
+        $("#highTemp").html(Math.round(maxTemp * 1.8 + 32) + "°F");
+        $("#lowTemp").html(Math.round(minTemp * 1.8 + 32) + "°F");
+        $(".temperatureStyle").html(Math.round(mainTemp * 1.8 + 32) + "°F");
+    }
 }
 
 
 /*ACCESS TO MAIN VALUES OF WEATHER API*/
 function weather(response) {
-    // console.log(response);
+    console.log(response);
+
+    mainTemp = Math.round(response.main.temp);
+    maxTemp = Math.round(response.main.temp_max);
+    minTemp = Math.round(response.main.temp_min);
 
     $("#cityStyle").html(response.name);
-    $("#highTemp").html(response.main.temp_max + "°");
-    $("#lowTemp").html(response.main.temp_min + "°");
-    $(".temperatureStyle").html(Math.round(response.main.temp) + "°");
+    $("#highTemp").html(maxTemp + "°C");
+    $("#lowTemp").html(minTemp + "°C");
+    $(".temperatureStyle").html(mainTemp + "°C");
     $(".descriptionStyle").html(response.weather[0].main);
 
 
     /*CUSTOMIZE IMAGES & BACKGROUND*/
 
-    var weatherDescription = response.weather[0].description;
+    var weatherDescription = response.weather[0].icon;
 
-    if (weatherDescription == "clear sky") {
+    if (weatherDescription == "01d" || weatherDescription == "01n") {
 
         $("#image").attr("src", "images/clear_sky.png");
         $(".backgroundColor").css("background", "#66ccff");
 
-    } else if (weatherDescription == "few clouds") {
+    } else if (weatherDescription == "02d" || weatherDescription == "02n") {
 
         $("#image").attr("src", "images/few_clouds.png");
         $(".backgroundColor").css("background", "#c4bfb9");
 
-    } else if (weatherDescription == "scattered clouds") {
+    } else if (weatherDescription == "03d" || weatherDescription == "03n") {
 
         $("#image").attr("src", "images/scattered_clouds.png");
         $(".backgroundColor").css("background", "#c4bfb9");
 
-    } else if (weatherDescription == "broken clouds"){
+    } else if (weatherDescription == "04d" || weatherDescription == "04n") {
 
-        $("#images").attr("src", "images/broken_clouds.png");
+        $("#image").attr("src", "images/broken_clouds.png");
         $(".backgroundColor").css("background", "#aea79e");
 
-    } else if (weatherDescription == "shower rain") {
+    } else if (weatherDescription == "09d" || weatherDescription == "09n") {
 
         $("#image").attr("src", "images/shower_rain.png");
         $(".backgroundColor").css("background", "#a29a90");
 
-    } else if (weatherDescription == "rain") {
+    } else if (weatherDescription == "10d" || weatherDescription == "10n") {
 
         $("#image").attr("src", "images/rain.png");
         $(".backgroundColor").css("background", "#C0C0C0");
 
-    } else if (weatherDescription == "thunderstorm") {
+    } else if (weatherDescription == "11d" || weatherDescription == "11n") {
 
         $("#image").attr("src", "images/thunderstorm.png");
         $(".backgroundColor").css("background", "#978d82");
 
-    } else if (weatherDescription == "snow") {
+    } else if (weatherDescription == "13d" || weatherDescription == "13n") {
 
         $("#image").attr("src", "images/snow.png");
         $(".backgroundColor").css("background", "#d1cdc7");
 
+    } else if (weatherDescription == "50d" || weatherDescription == "50n") {
+
+        $("#image").attr("src", "images/mist.png");
+        $(".backgroundColor").css("background", "#b3b3b3");
     }
 }
 
@@ -87,4 +126,7 @@ function weather(response) {
 $(document).ready(function () {
     navigator.geolocation.getCurrentPosition(position);
     setTime();
+    // $("#celsButton").change(changeUnits());
+
+    $("#celsButton").click(changeUnits);
 });
